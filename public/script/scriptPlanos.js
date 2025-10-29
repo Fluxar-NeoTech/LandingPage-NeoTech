0878c5180d72ce361c1fa29d415be3d9539b7e8b
Gustavo Sousa De Oliveira <gustavosousa-ieg@instituto.ad>
2025-10-28 23:56:14 -0300

    Responsividade-Atualizada-Membros-Adicionado

---
+++ public/script/scriptPlanos.js
@@ -0,0 +1,17 @@
+function mudarConteudoMENSAL() {
+    document.getElementById("p1").innerHTML = "599,99";
+    document.getElementById("p2").innerHTML = "899,99";
+    document.getElementById("p3").innerHTML = "1599,99";
+    document.getElementById("pp1").innerHTML = "mensal";
+    document.getElementById("pp2").innerHTML = "mensal";
+    document.getElementById("pp3").innerHTML = "mensal";
+}
+
+function mudarConteudoANUAL() {
+    document.getElementById("p1").innerHTML = "499,00";
+    document.getElementById("p2").innerHTML = "799,00";
+    document.getElementById("p3").innerHTML = "1499,00";
+    document.getElementById("pp1").innerHTML = "anual";
+    document.getElementById("pp2").innerHTML = "anual";
+    document.getElementById("pp3").innerHTML = "anual";
+}
